36f8c764c179840803b298545e744b1591d580ae
Oğuzhan Arı <oguzhari@gmail.com>
2024-12-31 10:49:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1887,7 +1887,7 @@ def versiyon():
     st.caption(
         """
                 <p style='text-align: center;'>
-                ver 2.0.0_beta<br/><font size="2">build 30122024.2216</font>
+                ver 2.0.0_beta<br/><font size="2">build 31122024.1050</font>
                 </p>
             """,
         unsafe_allow_html=True,
